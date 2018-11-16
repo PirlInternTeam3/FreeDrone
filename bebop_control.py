f6b4a2c28048ee9c8c6f2236eddb3f0274cba523
gunhokim <ghk9204@gmail.com>
2018-11-16 13:59:37 +0900

    keyboard control test

--- bebop_control.py
+++ bebop_control.py
@@ -19,20 +19,33 @@ bebop.ask_for_state_update()
 
 bebop.safe_takeoff(5)
 
-print("Flying direct: going forward (positive pitch)") #전진
-bebop.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=1)
+# print("Flying direct: going forward (positive pitch)") #전진
+# bebop.fly_direct(roll=0, pitch=20, yaw=0, vertical_movement=0, duration=0.1)
+#
+# print("Flying direct: yaw") #회전
+# bebop.fly_direct(roll=0, pitch=0, yaw=20, vertical_movement=0, duration=0.1)
+#
+# print("Flying direct: going backwards (negative pitch)") #후진
+# bebop.fly_direct(roll=0, pitch=-20, yaw=0, vertical_movement=0, duration=3)
+#
+# print("Flying direct: roll") #좌우
+# bebop.fly_direct(roll=20, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+#
+# print("Flying direct: going up") #업
+# bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=10, duration=0.1)
 
-print("Flying direct: yaw") #회전
-bebop.fly_direct(roll=0, pitch=0, yaw=50, vertical_movement=0, duration=1)
 
-print("Flying direct: going backwards (negative pitch)") #후진
-bebop.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.5)
+while True:
+    inp = input("input command")
 
-print("Flying direct: roll") #좌우
-bebop.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=1)
+    if inp == "w":
+        bebop.fly_direct(roll=0, pitch=20, yaw=0, vertical_movement=0, duration=0.5)
+    if inp == "s":
+        bebop.fly_direct(roll=0, pitch=-20, yaw=0, vertical_movement=0, duration=0.5)
+    if inp == "p":
+        breakwwwwwww
 
-print("Flying direct: going up") #업
-bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=50, duration=1)
+
 
 #print("Turning relative")
 #bebop.move_relative(0, 0, 0, math.radians(90))
