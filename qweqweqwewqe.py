88b088835ff2bc1770f0e480bdf09e60f38503b1
GeonKim <ghk9204@gmail.com>
2018-11-23 11:28:55 +0900

    pygame controller saving img test (test.py)

--- qweqweqwewqe.py
+++ qweqweqwewqe.py
@@ -32,11 +32,6 @@ class UserVision:
             print("No img...")
 
 def demo_user_code_after_vision_opened(bebopVision, args):
-    bebop = args[0]
-
-    print("Vision successfully started!")
-    # removed the user call to this function (it now happens in open_video())
-    # bebopVision.start_video_buffering()
 
     # initializes Pygame
     pygame.init()
@@ -47,26 +42,23 @@ def demo_user_code_after_vision_opened(bebopVision, args):
     # sets the window size
     pygame.display.set_mode((400, 400))
 
-    #
+    # repeat key input
     pygame.key.set_repeat(True)
 
-    bebop = Bebop()
 
-    print("connecting")
-    success = bebop.connect(10)
-    print(success)
+    bebop = args[0]
 
-    print("sleeping")
-    bebop.smart_sleep(5)
-
-    bebop.ask_for_state_update()
+    print("Vision successfully started!")
+    # removed the user call to this function (it now happens in open_video())
+    # bebopVision.start_video_buffering()
 
     # takeoff
-    bebop.safe_takeoff(5)
+    # bebop.safe_takeoff(5)
 
     # skipping actually flying for safety purposes indoors - if you want
     # different pictures, move the bebop around by hand
-    bebop.smart_sleep(10)
+    print("Fly me around by hand!")
+    bebop.smart_sleep(5)
 
     if (bebopVision.vision_running):
 
@@ -92,22 +84,31 @@ def demo_user_code_after_vision_opened(bebopVision, args):
                 if event.type == pygame.KEYDOWN:
                     # prints on the console the key pressed
                     if key_name == "UP":
-                        bebop.fly_direct(roll=0, pitch=20, yaw=0, vertical_movement=0, duration=0.1)
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=40, yaw=0, vertical_movement=0, duration=0.1)
                     elif key_name == "DOWN":
-                        bebop.fly_direct(roll=0, pitch=-20, yaw=0, vertical_movement=0, duration=0.1)
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=-40, yaw=0, vertical_movement=0, duration=0.1)
                     elif key_name == "LEFT":
-                        bebop.fly_direct(roll=-20, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+                        print()
+                        #bebop.fly_direct(roll=-40, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
                     elif key_name == "RIGHT":
-                        bebop.fly_direct(roll=20, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+                        print()
+                        #bebop.fly_direct(roll=40, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
                     elif key_name == "W":
-                        bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=10, duration=0.1)
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=15, duration=0.1)
                     elif key_name == "S":
-                        bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-10, duration=0.1)
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-15, duration=0.1)
                     elif key_name == "A":
-                        bebop.fly_direct(roll=0, pitch=0, yaw=-60, vertical_movement=0, duration=0.1)
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=0, yaw=-100, vertical_movement=0, duration=0.1)
                     elif key_name == "D":
-                        bebop.fly_direct(roll=0, pitch=0, yaw=60, vertical_movement=-0, duration=0.1)
+                        print()
+                        # bebop.fly_direct(roll=0, pitch=0, yaw=100, vertical_movement=-0, duration=0.1)
                     print(u'"{}" key pressed'.format(key_name))
+
 
                 # if any key is released
                 elif event.type == pygame.KEYUP:
@@ -117,10 +118,10 @@ def demo_user_code_after_vision_opened(bebopVision, args):
         # finalizes Pygame
         pygame.quit()
 
-        bebop.smart_sleep(1)
+        bebop.smart_sleep(5)
 
         # land
-        bebop.safe_land(5)
+        # bebop.safe_land(5)
 
         print("Finishing demo and stopping vision")
         bebopVision.close_video()
@@ -143,6 +144,7 @@ if __name__ == "__main__":
 
         userVision = UserVision(bebopVision)
         bebopVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None)
+
         bebopVision.open_video()
 
     else:
--- test.py
+++ test.py
@@ -1,12 +1,38 @@
-# coding=utf-8
-
-# imports the Pygame library
-import pygame
+"""
+Demo of the Bebop vision using DroneVisionGUI (relies on libVLC).
+multi-threaded approach than DroneVision
+It is a different
+Author: Amy McGovern
+"""
 from pyparrot.Bebop import Bebop
-import math
+from pyparrot.DroneVisionGUI import DroneVisionGUI
+import threading
+import cv2
+import time
+import pygame
 
+isAlive = False
 
-def main():
+class UserVision:
+    def __init__(self, vision):
+        self.index = 0
+        self.vision = vision
+
+    def save_pictures(self, args):
+
+        print("saving picture")
+        img = self.vision.get_latest_valid_picture()
+
+        if (img is not None):
+            filename = "./images/bebop2/test_image_%06d.png" % self.index
+            print("filename:", filename)
+            cv2.imwrite(filename, img)
+            self.index += 1
+        else:
+            print("No img...")
+
+def demo_user_code_after_vision_opened(bebopVision, args):
+
     # initializes Pygame
     pygame.init()
 
@@ -16,76 +42,110 @@ def main():
     # sets the window size
     pygame.display.set_mode((400, 400))
 
-    #
+    # repeat key input
     pygame.key.set_repeat(True)
 
-    bebop = Bebop()
 
-    print("connecting")
-    success = bebop.connect(10)
-    print(success)
+    bebop = args[0]
 
-    print("sleeping")
+    print("Vision successfully started!")
+    # removed the user call to this function (it now happens in open_video())
+    # bebopVision.start_video_buffering()
+
+    # takeoff
+    # bebop.safe_takeoff(5)
+
+    # skipping actually flying for safety purposes indoors - if you want
+    # different pictures, move the bebop around by hand
+    print("Fly me around by hand!")
     bebop.smart_sleep(5)
 
-    bebop.ask_for_state_update()
+    if (bebopVision.vision_running):
 
-    bebop.safe_takeoff(5)
+        # infinite loop
+        while True:
+            # gets a single event from the event queue
+            event = pygame.event.wait()
+
+            # if the 'close' button of the window is pressed
+            if event.type == pygame.QUIT:
+                # stops the application
+                break
+
+            # cptures the 'KEYDOWN' and 'KEYUP' events
+            if event.type in (pygame.KEYDOWN, pygame.KEYUP):
+                # gets the key name
+                key_name = pygame.key.name(event.key)
+
+                # converts to uppercase the key name
+                key_name = key_name.upper()
+
+                # if any key is pressed
+                if event.type == pygame.KEYDOWN:
+                    # prints on the console the key pressed
+                    if key_name == "UP":
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=40, yaw=0, vertical_movement=0, duration=0.1)
+                    elif key_name == "DOWN":
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=-40, yaw=0, vertical_movement=0, duration=0.1)
+                    elif key_name == "LEFT":
+                        print()
+                        #bebop.fly_direct(roll=-40, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+                    elif key_name == "RIGHT":
+                        print()
+                        #bebop.fly_direct(roll=40, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
+                    elif key_name == "W":
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=15, duration=0.1)
+                    elif key_name == "S":
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-15, duration=0.1)
+                    elif key_name == "A":
+                        print()
+                        #bebop.fly_direct(roll=0, pitch=0, yaw=-100, vertical_movement=0, duration=0.1)
+                    elif key_name == "D":
+                        print()
+                        # bebop.fly_direct(roll=0, pitch=0, yaw=100, vertical_movement=-0, duration=0.1)
+                    print(u'"{}" key pressed'.format(key_name))
 
 
-    # infinite loop
-    while True:
-        # gets a single event from the event queue
-        event = pygame.event.wait()
+                # if any key is released
+                elif event.type == pygame.KEYUP:
+                    # prints on the console the released key
+                    print(u'"{}" key released'.format(key_name))
 
-        # if the 'close' button of the window is pressed
-        if event.type == pygame.QUIT:
-            # stops the application
-            break
+        # finalizes Pygame
+        pygame.quit()
 
-        # cptures the 'KEYDOWN' and 'KEYUP' events
-        if event.type in (pygame.KEYDOWN, pygame.KEYUP):
-            # gets the key name
-            key_name = pygame.key.name(event.key)
+        bebop.smart_sleep(5)
 
-            # converts to uppercase the key name
-            key_name = key_name.upper()
+        # land
+        # bebop.safe_land(5)
 
-            # if any key is pressed
-            if event.type == pygame.KEYDOWN:
-                # prints on the console the key pressed
-                if key_name == "UP":
-                    bebop.fly_direct(roll=0, pitch=20, yaw=0, vertical_movement=0, duration=0.1)
-                elif key_name == "DOWN":
-                    bebop.fly_direct(roll=0, pitch=-20, yaw=0, vertical_movement=0, duration=0.1)
-                elif key_name == "LEFT":
-                    bebop.fly_direct(roll=-20, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
-                elif key_name == "RIGHT":
-                    bebop.fly_direct(roll=20, pitch=0, yaw=0, vertical_movement=0, duration=0.1)
-                elif key_name == "W":
-                    bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=10, duration=0.1)
-                elif key_name == "S":
-                    bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-10, duration=0.1)
-                elif key_name == "A":
-                    bebop.fly_direct(roll=0, pitch=0, yaw=-60, vertical_movement=0, duration=0.1)
-                elif key_name == "D":
-                    bebop.fly_direct(roll=0, pitch=0, yaw=60, vertical_movement=-0, duration=0.1)
-                print(u'"{}" key pressed'.format(key_name))
+        print("Finishing demo and stopping vision")
+        bebopVision.close_video()
 
-            # if any key is released
-            elif event.type == pygame.KEYUP:
-                # prints on the console the released key
-                print(u'"{}" key released'.format(key_name))
-
-    # finalizes Pygame
-    pygame.quit()
-
-    bebop.smart_sleep(1)
-    bebop.safe_land(5)
-
-    print("DONE - disconnecting")
+    # disconnect nicely so we don't need a reboot
+    print("disconnecting")
     bebop.disconnect()
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    # make my bebop object
+    bebop = Bebop()
+
+    # connect to the bebop
+    success = bebop.connect(5)
+
+    if (success):
+        # start up the video
+        bebopVision = DroneVisionGUI(bebop, is_bebop=True, user_code_to_run=demo_user_code_after_vision_opened, user_args=(bebop,))
+
+        userVision = UserVision(bebopVision)
+        bebopVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None)
+
+        bebopVision.open_video()
+
+    else:
+        print("Error connecting to bebop. Retry")
