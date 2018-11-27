9310d71f94c46ea14641f9ff5a3e1884389f7c65
danny <92dannylee@gmail.com>
2018-11-28 03:54:26 +0900

    cleanup all data and set default

---
+++ cleanup_all_data_and_set_default.py
@@ -0,0 +1,8 @@
+#-*-coding:UTF-8-*-
+
+import os
+
+os.system('cp -r ./cleanup/images ./')
+os.system('cp -r ./cleanup/test_data ./')
+os.system('cp -r ./cleanup/train_data ./')
+os.system('cp -r ./cleanup/checkpoint ./')
