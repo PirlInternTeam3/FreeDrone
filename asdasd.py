0314dbccb1530fefb675a79f64e583f2886ff8d3
danny3314 <92dannylee@gmail.com>
2018-12-15 22:05:05 +0900

    YOLO tracker 추가

---
+++ asdasd.py
@@ -0,0 +1,184 @@
+import cv2
+import time
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from core import utils
+
+from pyparrot.Minidrone import Mambo
+from pyparrot.DroneVisionGUI import DroneVisionGUI
+
+class UserVision:
+
+    def __init__(self, vision):
+        self.index = 0
+        self.vision = vision
+
+    def save_pictures(self, args):
+        #print("saving picture")
+        img = self.vision.get_latest_valid_picture()
+
+        if (img is not None):
+            #filename = "test_image_%06d.png" % self.index
+            #cv2.imwrite(filename, img)
+            self.index +=1
+
+class FreeDrone(object):
+
+    def __init__(self):
+        self.contours = None
+
+    def start(self):
+        # mamboAddr = "e0:14:d0:63:3d:d0"
+        mamboAddr = "88:36:6C:F1:E9:47"
+
+        self.mambo = Mambo(mamboAddr, use_wifi=True)
+        success = self.mambo.connect(num_retries=5)
+
+        #        #print("sleeping")
+        #        self.mambo.smart_sleep(5)
+        #        self.mambo.ask_for_state_update()
+        #        self.mambo.safe_takeoff(5)
+
+        if (success):
+            print("success")
+            self.mamboVision = DroneVisionGUI(self.mambo, is_bebop=False,
+                                              user_code_to_run=self.demo_user_code_after_vision_opened,
+                                              user_args=(self.mambo,))
+            self.userVision = UserVision(self.mamboVision)
+            self.mamboVision.set_user_callback_function(self.userVision.save_pictures, user_callback_args=None)
+            self.mamboVision.open_video()
+
+    def demo_user_code_after_vision_opened(self, mamboVision, args):
+        self.mambo = args[0]
+        print("Vision Successfully Started!")
+
+        if (self.mamboVision.vision_running):
+            print("Moving the camera using velocity")
+
+            SIZE = [416, 416]
+            classes = utils.read_coco_names('./data/coco.names')
+            num_classes = len(classes)
+            input_tensor, output_tensors = utils.read_pb_return_tensors(tf.get_default_graph(),
+                                                                        "./checkpoint/yolov3_cpu_nms.pb",
+                                                                        ["Placeholder:0", "concat_9:0", "mul_9:0"])
+
+            with tf.Session() as sess:
+                while True:
+                    IMG = self.mamboVision.img
+                    image = Image.fromarray(self.mamboVision.img)
+
+                    img_resized = np.array(image.resize(size=tuple(SIZE)), dtype=np.float32)
+                    img_resized = img_resized / 255.
+                    prev_time = time.time()
+
+                    boxes, scores = sess.run(output_tensors, feed_dict={input_tensor: np.expand_dims(img_resized, axis=0)})
+                    boxes, scores, labels = utils.cpu_nms(boxes, scores, num_classes, score_thresh=0.4, iou_thresh=0.5)
+
+                    print(labels)
+                    print(boxes)
+
+                    try:
+                        #label number for person -> 0
+                        l=[]
+                        b=[]
+                        for i in range(len(labels)):
+                            if labels[i] == 0:
+                                l.append(labels[i])
+                                b.append(boxes[i])
+                            else:
+                                break
+
+                        print("labels: ", labels)
+                        print("l: ", l)
+
+                        image, bbox = utils.draw_boxes(image, boxes, scores, l, classes, SIZE, show=False)
+
+                        print("lengths of bbox: ",len(bbox))
+
+                        for j in range(len(bbox)):
+
+                            x1 = int(bbox[j][0])
+                            y1 = int(bbox[j][1])
+                            x2 = int(bbox[j][2])
+                            y2 = int(bbox[j][3])
+
+
+                            image_roi = IMG[y1:y2, x1:x2]
+
+                            lower_red_1 = np.array([17, 15, 100])
+                            upper_red_1 = np.array([50, 56, 200])
+
+                            try:
+                                # 해당 영역에서 Red color를 찾는다.
+                                image_roi = cv2.inRange(image_roi, lower_red_1, upper_red_1)
+
+                                # 노이즈를 줄이기 위해 블러링 한다
+                                image_roi = cv2.medianBlur(image_roi, 11)
+
+                                # Find contours and decide if hat is one of them
+                                # find contours 함수는 원본 이미지를 직접 수정하기 때문에 원본이미지 보존을 위해 .copy 사용한다.
+                                # cv2.RETR_TREE 는 모든 contours line을 찾으며, 모든 hieracy관계를 구성함.
+                                # contour를 찾을때 근사치 찾는 방법으로 APPROX_SIMPLE은 contours line을 그릴 수 있는 point 만 저장
+                                # Returns:	image, contours , hierachy
+                                _, contours, hierarchy = cv2.findContours(image_roi.copy(), cv2.RETR_TREE,
+                                                                          cv2.CHAIN_APPROX_SIMPLE)
+
+                                if contours:
+                                    w = x_w - x
+                                    h = y_h - y
+
+                                    Area = w * h
+                                    x_centroid = x + (w / 2)
+                                    y_centroid = y + (h / 2)
+
+                                    #cv2.drawContours(image, self.contours, -1, (0, 255, 0), 3)
+                                    cv2.rectangle(IMG, (x, y), (x + w, y + h), [0,255,0], 2)
+                                    print("Box Values x: %s, y: %s, x+w: %s, y+h: %s" % (x, y, x_w, y_h))
+                                    print(Area)
+
+                                    if Area < 30000:
+                                        print("Forward (positive pitch)")
+                                        #self.mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=0.5)
+
+                                    elif Area > 60000:
+                                        print("Backwards (negative pitch)")
+                                        #self.mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.5)
+
+                                    # x divide into three spaces -> 0-285, 285 - 570, 570 - 856 (x:856)
+                                    # y divide into three spaces -> 0-160, 160 - 320, 320 - 480 (y:480)
+                                    if x_centroid > 450:  # the object is on the right side (clockwise), move right a little bit
+                                        print("Clockwise-Right (positive yaw)")
+                                        #self.mambo.fly_direct(roll=0, pitch=0, yaw=50, vertical_movement=0, duration=1)
+
+                                    elif x_centroid < 350:  # the object is on the left side (counter-clockwise), move left a little bit
+                                        print("Counterclockwise-Left (negative yaw)")
+                                        #self.mambo.fly_direct(roll=0, pitch=0, yaw=-50, vertical_movement=0, duration=1)
+
+                            except:
+                               print("No Person with Red")
+                               continue
+
+                    except:
+                        print("No Person to Detect")
+                        continue
+
+                    curr_time = time.time()
+                    exec_time = curr_time - prev_time
+                    result = np.asarray(image)
+                    info = "time: %.2f ms" %(1000*exec_time)
+                    cv2.putText(result, text=info, org=(50, 70), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                                fontScale=1, color=(255, 0, 0), thickness=2)
+
+                    #cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
+                    cv2.imshow("cam", result)
+                    if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+
+def main():
+    drone = FreeDrone()
+    drone.start()
+
+
+if __name__ == '__main__':
+    main()
--- yolo_mambo.py
+++ yolo_mambo.py
@@ -1,200 +1,209 @@
-#! /usr/bin/env python
-# coding=utf-8
-#================================================================
-#   Copyright (C) 2018 * Ltd. All rights reserved.
-#
-#   Editor      : VIM
-#   File name   : video_demo.py
-#   Author      : YunYang1994
-#   Created date: 2018-11-30 15:56:37
-#   Description :
-#
-#================================================================
-
 import cv2
-import time
-import numpy as np
 import tensorflow as tf
 from PIL import Image
 from core import utils
-
+import time
+import numpy as np
+from scipy.spatial import distance
 from pyparrot.Minidrone import Mambo
 from pyparrot.DroneVisionGUI import DroneVisionGUI
 
-# #video_path = "./data/demo_data/road.mp4"
-# #video_path = 0 # use camera
+import os
+
+import argparse
+import glob
+import sys
+
+
+
+SIZE = [416, 416]
+#video_path = "./data/demo_data/road.mp4"
+video_path = 0 # use camera
+classes = utils.read_coco_names('./data/coco.names')
+num_classes = len(classes)
+input_tensor, output_tensors = utils.read_pb_return_tensors(tf.get_default_graph(), "./checkpoint/yolov3_gpu_nms.pb",
+                                           ["Placeholder:0", "concat_10:0", "concat_11:0", "concat_12:0"])
+
+
+height = 360
+width = 640
+# drone_centroid
+drone_centroid = (int(width / 2), int(height / 2))
+
+
+
 
 class UserVision:
-
     def __init__(self, vision):
         self.index = 0
         self.vision = vision
 
     def save_pictures(self, args):
-        #print("saving picture")
+        # print("in save pictures on image %d " % self.index)
+
         img = self.vision.get_latest_valid_picture()
 
         if (img is not None):
-            #filename = "test_image_%06d.png" % self.index
-            #cv2.imwrite(filename, img)
+            filename = "./images/mambo/test_image_%06d.png" % self.index
+            # uncomment this if you want to write out images every time you get a new one
+            # cv2.imwrite(filename, img)
             self.index +=1
-
-class YOLODrone(object):
-
-    def __init__(self):
-        self.contours = None
-
-    def start(self):
-        # mamboAddr = "e0:14:d0:63:3d:d0"
-        mamboAddr = "88:36:6C:F1:E9:47"
-
-        self.mambo = Mambo(mamboAddr, use_wifi=True)
-        success = self.mambo.connect(num_retries=5)
-
-        #        #print("sleeping")
-        #        self.mambo.smart_sleep(5)
-        #        self.mambo.ask_for_state_update()
-        #        self.mambo.safe_takeoff(5)
-
-        if (success):
-            print("success")
-            self.mamboVision = DroneVisionGUI(self.mambo, is_bebop=False,
-                                              user_code_to_run=self.demo_user_code_after_vision_opened,
-                                              user_args=(self.mambo,))
-            self.userVision = UserVision(self.mamboVision)
-            self.mamboVision.set_user_callback_function(self.userVision.save_pictures, user_callback_args=None)
-            self.mamboVision.open_video()
-
-    def demo_user_code_after_vision_opened(self, mamboVision, args):
-        self.mambo = args[0]
-        print("Vision Successfully Started!")
-
-        if (self.mamboVision.vision_running):
-            print("Moving the camera using velocity")
-
-            SIZE = [416, 416]
-            classes = utils.read_coco_names('./data/coco.names')
-            num_classes = len(classes)
-            input_tensor, output_tensors = utils.read_pb_return_tensors(tf.get_default_graph(),
-                                                                        "./checkpoint/yolov3_cpu_nms.pb",
-                                                                        ["Placeholder:0", "concat_9:0", "mul_9:0"])
-
-            with tf.Session() as sess:
-                while True:
-                    IMG = self.mamboVision.img
-                    image = Image.fromarray(self.mamboVision.img)
-
-                    img_resized = np.array(image.resize(size=tuple(SIZE)), dtype=np.float32)
-                    img_resized = img_resized / 255.
-                    prev_time = time.time()
-
-                    boxes, scores = sess.run(output_tensors, feed_dict={input_tensor: np.expand_dims(img_resized, axis=0)})
-                    boxes, scores, labels = utils.cpu_nms(boxes, scores, num_classes, score_thresh=0.4, iou_thresh=0.5)
-
-                    print(labels)
-                    print(boxes)
-
-                    try:
-                        #label number for person -> 0
-                        l=[]
-                        b=[]
-                        for i in range(len(labels)):
-                            if labels[i] == 0:
-                                l.append(labels[i])
-                                b.append(boxes[i])
-                            else:
-                                break
-
-                        print("labels: ", labels)
-                        print("l: ", l)
-
-                        image, bbox = utils.draw_boxes(image, boxes, scores, l, classes, SIZE, show=False)
-
-                        print("lengths of bbox: ",len(bbox))
-
-                        for j in range(len(bbox)):
-
-                            x = int(bbox[j][0])
-                            y = int(bbox[j][1])
-                            x_w = int(bbox[j][2])
-                            y_h = int(bbox[j][3])
-
-                            print("x: ", x)
-                            print("y: ", y)
-                            print("x+w: ", x_w)
-                            print("y+h: ", y_h)
-
-                            image_roi = IMG[y:y_h, x:x_w]
-
-                            lower_red_1 = np.array([17, 15, 100])
-                            upper_red_1 = np.array([50, 56, 200])
-
-                            try:
-
-                                image_roi = cv2.inRange(image_roi, lower_red_1, upper_red_1)
-
-                                # Put on median blur to reduce noise
-                                image_roi = cv2.medianBlur(image_roi, 11)
-
-                                # Find contours and decide if hat is one of them
-                                _, contours, hierarchy = cv2.findContours(image_roi.copy(), cv2.RETR_TREE,
-                                                                          cv2.CHAIN_APPROX_SIMPLE)
-
-                                if contours:
-                                    w = x_w - x
-                                    h = y_h - y
-
-                                    Area = w * h
-                                    x_centroid = x + (w / 2)
-                                    y_centroid = y + (h / 2)
-
-                                    #cv2.drawContours(image, self.contours, -1, (0, 255, 0), 3)
-                                    cv2.rectangle(IMG, (x, y), (x + w, y + h), [0,255,0], 2)
-                                    print("Box Values x: %s, y: %s, x+w: %s, y+h: %s" % (x, y, x_w, y_h))
-                                    print(Area)
-
-                                    if Area < 30000:
-                                        print("Forward (positive pitch)")
-                                        #self.mambo.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=0.5)
-
-                                    elif Area > 60000:
-                                        print("Backwards (negative pitch)")
-                                        #self.mambo.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=0.5)
-
-                                    # x divide into three spaces -> 0-285, 285 - 570, 570 - 856 (x:856)
-                                    # y divide into three spaces -> 0-160, 160 - 320, 320 - 480 (y:480)
-                                    if x_centroid > 450:  # the object is on the right side (clockwise), move right a little bit
-                                        print("Clockwise-Right (positive yaw)")
-                                        #self.mambo.fly_direct(roll=0, pitch=0, yaw=50, vertical_movement=0, duration=1)
-
-                                    elif x_centroid < 350:  # the object is on the left side (counter-clockwise), move left a little bit
-                                        print("Counterclockwise-Left (negative yaw)")
-                                        #self.mambo.fly_direct(roll=0, pitch=0, yaw=-50, vertical_movement=0, duration=1)
-
-                            except:
-                               print("No Person with Red")
-                               continue
-
-                    except:
-                        print("No Person to Detect")
-                        continue
-
-                    curr_time = time.time()
-                    exec_time = curr_time - prev_time
-                    result = np.asarray(image)
-                    info = "time: %.2f ms" %(1000*exec_time)
-                    cv2.putText(result, text=info, org=(50, 70), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                fontScale=1, color=(255, 0, 0), thickness=2)
-
-                    #cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
-                    cv2.imshow("cam", result)
-                    if cv2.waitKey(1) & 0xFF == ord('q'): break
+            #print(self.index)
 
 
-def main():
-    drone = YOLODrone()
-    drone.start()
+def tracking_target(mamboVision, args):
+
+    mambo = args[0]
+
+    if args[1] == 't':
+        testFlying = True
+    else :
+        testFlying = False
+
+    if (testFlying):
+        mambo.safe_takeoff(5)
+
+    with tf.Session() as sess:
+        while True:
+            image = mamboVision.img.copy()
+
+            if image is None:
+                print("드론으로부터 전달받은 이미지 없음!")
+
+            else :
+                img_resized = np.array(image.resize(size=tuple(SIZE)), dtype=np.float32)
+                img_resized = img_resized / 255.
+                prev_time = time.time()
+
+                boxes, scores, labels = sess.run(output_tensors,
+                                                 feed_dict={input_tensor: np.expand_dims(img_resized, axis=0)})
+                image = utils.draw_boxes(image, boxes, scores, labels, classes, SIZE, show=False)
 
 
-if __name__ == '__main__':
-    main()
+                curr_time = time.time()
+                exec_time = curr_time - prev_time
+                result = np.asarray(image)
+                info = "time: %.2f ms" % (1000 * exec_time)
+                cv2.putText(result, text=info, org=(50, 70), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=1, color=(255, 0, 0), thickness=2)
+                cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
+                cv2.imshow("result", result)
+                if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+
+    while True:
+        mambo_img = mamboVision.img.copy()
+
+        if mousedown:
+            cv2.rectangle(mambo_img,
+                          (int(boxToDraw[0]), int(boxToDraw[1])),  # point 1
+                          (int(boxToDraw[2]), int(boxToDraw[3])),  # point 2
+                          [0, 0, 255], PADDING)  # Color B G R 순서 , Thickness
+            if RECORD:
+                cv2.circle(mambo_img, (int(drawnBox[2]), int(drawnBox[3])), 10, [255, 0, 0], 4)
+
+            # 마우스 좌클릭을 떼고나면
+        elif mouseupdown:
+            if initialize:
+                outputBoxToDraw = tracker.track('Webcam', mambo_img[:, :, ::-1], boxToDraw)
+                initialize = False
+            else:
+                outputBoxToDraw = tracker.track('Webcam', mambo_img[:, :, ::-1])
+
+
+            # ROI 트래킹 유지
+            cv2.rectangle(mambo_img,
+                          (int(outputBoxToDraw[0]), int(outputBoxToDraw[1])),
+                          (int(outputBoxToDraw[2]), int(outputBoxToDraw[3])),
+                          color=[0, 0, 255], thickness=PADDING)
+
+            target_centroid = (int(outputBoxToDraw[0] + (outputBoxToDraw[2] - outputBoxToDraw[0]) / 2),
+                               int(outputBoxToDraw[1] + (outputBoxToDraw[3] - outputBoxToDraw[1]) / 2))
+
+            cv2.circle(mambo_img, target_centroid, radius=4, color=[0, 0, 255], thickness=PADDING)
+            cv2.circle(mambo_img, drone_centroid, radius=4, color=[255, 0, 0], thickness=PADDING)
+            cv2.arrowedLine(mambo_img, drone_centroid, target_centroid,
+                            color=[255, 0, 0], thickness=4)
+
+            dst = distance.euclidean(drone_centroid, target_centroid)
+
+            # pitch_rate = 0
+            # yaw_rate = 0
+            # vertical_rate = 0
+
+            if dst > 10:
+                pitch_rate = int(0 / 10)
+                yaw_rate = int(dst / 10)
+                vertical_rate = int(0 / 10)
+
+                # 우하단
+                if drone_centroid[0] <= target_centroid[0] and drone_centroid[1] <= target_centroid[1]:
+                    vertical_rate = -vertical_rate
+
+                # 좌하단
+                elif drone_centroid[0] > target_centroid[0] and drone_centroid[1] <= target_centroid[1]:
+                    yaw_rate = -yaw_rate
+                    vertical_rate = -vertical_rate
+
+                # 좌상단
+                elif drone_centroid[0] > target_centroid[0] and drone_centroid[1] > target_centroid[1]:
+                    yaw_rate = -yaw_rate
+
+            else:
+                pitch_rate = int(0 / 10)
+                yaw_rate = int(0 / 10)
+                vertical_rate = int(0 / 10)
+
+            print("dst: {}, pitch: {}/s, yaw: {}/s, vertical: {}/s".format(dst, pitch_rate, yaw_rate, vertical_rate))
+
+            if (testFlying):
+                mambo.fly_direct(roll=0, pitch=pitch_rate, yaw=yaw_rate, vertical_movement=vertical_rate, duration=1)
+
+        cv2.imshow('Webcam', mambo_img)
+
+        keyPressed = cv2.waitKey(1) and 0xFF
+        if keyPressed == ord("q"):
+            break
+
+        # cv2 종료
+    cv2.destroyAllWindows()
+
+    # land
+    if (testFlying):
+        mambo.safe_land(5)
+
+    # done doing vision demo
+    print("Ending the sleep and vision")
+    mamboVision.close_video()
+
+    mambo.smart_sleep(5)
+
+    print("disconnecting")
+    mambo.disconnect()
+
+
+if __name__ == "__main__":
+    # you will need to change this to the address of YOUR mambo
+    mamboAddr = "64:E5:99:F7:22:4A"
+
+    # make my mambo object
+    # remember to set True/False for the wifi depending on if you are using the wifi or the BLE to connect
+    mambo = Mambo(mamboAddr, use_wifi=True)
+    print("trying to connect to mambo now")
+    success = mambo.connect(num_retries=3)
+    print("connected: %s" % success)
+
+    if (success):
+        # get the state information
+        print("sleeping")
+        mambo.smart_sleep(1)
+        mambo.ask_for_state_update()
+        mambo.smart_sleep(1)
+
+        print("Preparing to open vision")
+        status = input("Input 't' if you want to TAKE OFF or not : ")
+        mamboVision = DroneVisionGUI(mambo, is_bebop=False, buffer_size=200,
+                                     user_code_to_run=tracking_target, user_args=(mambo, status))
+        userVision = UserVision(mamboVision)
+        #mamboVision.set_user_callback_function(userVision.save_pictures, user_callback_args=None)
+        mamboVision.open_video()
