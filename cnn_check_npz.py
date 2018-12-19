2bcf9ea7dde8d6b3c2593efe0df3977c9d0541f3
danny3314 <92dannylee@gmail.com>
2018-12-19 09:56:42 +0900

    라벨링 갯수 체크하는 코드

--- cnn_check_npz.py
+++ cnn_check_npz.py
@@ -9,6 +9,7 @@ training_data = glob.glob(path)
 height = 360
 width = 640
 
+frames = list()
 
 # load data
 for single_npz in training_data:
@@ -16,32 +17,13 @@ for single_npz in training_data:
             x = data['train']
             y = data['train_labels']
             y_df = pd.DataFrame(y, columns=['Forward', 'Right', 'Left'])
-            print(len(y))
-            print(len(y_df[y_df['Forward'] == 1]))
-            print(len(y_df[y_df['Right'] == 1]))
-            print(len(y_df[y_df['Left'] == 1]))
+            frames.append(y_df)
 
+result = pd.concat(frames)
 
-# #show img and label
-# def show_img_label(index):
-#
-#     plt.imshow(x[index].reshape(height, width))
-#
-#     label = np.argmax(y[index])
-#
-#     if label == 0:
-#         direction = 'Forward'
-#     elif label == 1:
-#         direction = 'Right'
-#     elif label == 2:
-#         direction = 'Left'
-#
-#     sub_plt_title = str(label) + " : " + direction
-#     plt.title(sub_plt_title)
-#     plt.show()
-#
-#
-# # 0번~끝까지 이미지 테스트 해보기
-#
-# for i in range(len(y)):
-#     show_img_label(i)
+total_num = len(result)
+forward_num = len(result[result['Forward'] == 1])
+right_num = len(result[result['Right'] == 1])
+left_num = len(result[result['Left'] == 1])
+
+print("Total # of Labels: {}\nForward: {}, Right: {}, Left: {}".format(total_num, forward_num, right_num, left_num))
